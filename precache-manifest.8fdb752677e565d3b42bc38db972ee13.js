7374c6a17b0f964ebec8c6ff4ed303d293aeaf28
nesso mohamed <nessomohamed555@gmail.com>
2020-02-28 18:37:55 +0200

    Updates

---
+++ precache-manifest.8fdb752677e565d3b42bc38db972ee13.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45a81df9f5890a3fa683022e3cfdc1eb",
+    "url": "/weather/index.html"
+  },
+  {
+    "revision": "d7aea85a3186f4223ac0",
+    "url": "/weather/static/css/2.61a82b33.chunk.css"
+  },
+  {
+    "revision": "e6d735f05f01ac187d7c",
+    "url": "/weather/static/css/main.df469fce.chunk.css"
+  },
+  {
+    "revision": "d7aea85a3186f4223ac0",
+    "url": "/weather/static/js/2.7e80e8d6.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/weather/static/js/2.7e80e8d6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e6d735f05f01ac187d7c",
+    "url": "/weather/static/js/main.fdcad40e.chunk.js"
+  },
+  {
+    "revision": "67e2127c387cb441f1df",
+    "url": "/weather/static/js/runtime-main.51e543ba.js"
+  },
+  {
+    "revision": "1f65df40a5b1fd0f706a723bdba10259",
+    "url": "/weather/static/media/weather20.1f65df40.jpg"
+  },
+  {
+    "revision": "1cd48d78f06d33973d9d761d426e69bf",
+    "url": "/weather/static/media/weathericons-regular-webfont.1cd48d78.woff2"
+  },
+  {
+    "revision": "4618f0de2a818e7ad3fe880e0b74d04a",
+    "url": "/weather/static/media/weathericons-regular-webfont.4618f0de.ttf"
+  },
+  {
+    "revision": "4b658767da6bd92ce2addb3ce512784d",
+    "url": "/weather/static/media/weathericons-regular-webfont.4b658767.eot"
+  },
+  {
+    "revision": "8cac70ebda3f23ce472110d9f21e8593",
+    "url": "/weather/static/media/weathericons-regular-webfont.8cac70eb.woff"
+  },
+  {
+    "revision": "ecaf8b481729b18f6a8494d9f691cdae",
+    "url": "/weather/static/media/weathericons-regular-webfont.ecaf8b48.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/recipes/precache-manifest.d43ce8a184a01d114ffb2dccf2f57694.js"
+  "/weather/precache-manifest.8fdb752677e565d3b42bc38db972ee13.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/recipes/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/weather/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
